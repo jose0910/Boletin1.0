79c8a660bf3a1f2b97cf0f19f970d259f054c173
dam1 <jvazquezalonso@danielcastelao.org>
2025-11-06 13:35:24 +0100

    POO

--- 1º DAM/progra/apuntes/POO/MainPersona.py
+++ 1º DAM/progra/apuntes/POO/MainPersona.py
@@ -4,4 +4,4 @@ persona1 = Persona("Laura Gómez", 28, "12345678Z", "Calle Mayor 12, Madrid", "E
 print(persona1.mostrar_informacion())
 print(persona1.comprobar_edad())
 print(persona1.comprobar_dni())
-print(persona1)
+print(persona1.comprobar_dni())
